a26d6b58616094c72ce30adc04d45210f12863d7
tracer-ics2017 <tracer@njuics.org>
2026-06-02 13:10:59 +0000

    >  compile 2413993 ubuntu Linux cursor 6.1.147 #1 SMP PREEMPT_DYNAMIC Tue Aug  5 21:01:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:10:59 up 18:34,  0 user,  load average: 0.21, 7.68, 17.98 23d171d9644f067bc2e94b1b5ded2c72af4d50bd

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -9,20 +9,48 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t len) {
-  return 0;
+  int key = _read_key();
+  if (key != _KEY_NONE) {
+    int down = (key & 0x8000) != 0;
+    int code = key & ~0x8000;
+    return snprintf(buf, len, "k%c %s\n", down ? 'd' : 'u', keyname[code]);
+  }
+  return snprintf(buf, len, "t %lu\n", _uptime());
 }
 
 static char dispinfo[128] __attribute__((used));
 
-void dispinfo_read(void *buf, off_t offset, size_t len) {
+size_t dispinfo_read(void *buf, off_t offset, size_t len) {
+  size_t dlen = strlen(dispinfo);
+  if ((size_t)offset >= dlen) return 0;
+  size_t n = len;
+  if ((size_t)offset + n > dlen) n = dlen - offset;
+  memcpy(buf, dispinfo + offset, n);
+  return n;
 }
 
-void fb_write(const void *buf, off_t offset, size_t len) {
+size_t fb_write(const void *buf, off_t offset, size_t len) {
+  int w = _screen.width;
+  int px = offset / 4;
+  int x = px % w;
+  int y = px / w;
+  int pixel_count = len / 4;
+  if (pixel_count <= 0) return 0;
+
+  const uint32_t *pixels = buf;
+  int written = 0;
+  while (written < pixel_count) {
+    int n = w - x;
+    if (n > pixel_count - written) n = pixel_count - written;
+    _draw_rect(pixels + written, x, y, n, 1);
+    written += n;
+    x = 0;
+    y++;
+  }
+  return len;
 }
 
 void init_device() {
   _ioe_init();
-
-  // TODO: print the string to array `dispinfo` with the format
-  // described in the Navy-apps convention
+  snprintf(dispinfo, sizeof(dispinfo), "WIDTH:%d\nHEIGHT:%d\n", _screen.width, _screen.height);
 }

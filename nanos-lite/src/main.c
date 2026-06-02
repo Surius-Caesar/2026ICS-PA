ba8308eee351fc3703844a711343edace043a4f5
tracer-ics2017 <tracer@njuics.org>
2026-06-02 16:34:29 +0800

    >  run 2413993 surius Linux Surius 6.17.0-22-generic #22~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 26 15:25:54 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:34:29 up  3:51,  1 user,  load average: 0.16, 0.23, 0.26 a2cc3f89a2c75424fa143085f51394ce90a5e6e

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -31,7 +31,7 @@ int main() {
   init_fs();
 
   // Load user program by filename from filesystem
-  const char *program = "/bin/bmptest";  // Use first file in ramdisk for testing
+  const char *program = "/bin/text";  // Test filesystem with text program
   Log("Loading program: %s", program);
   uint32_t entry = loader(NULL, program);
   ((void (*)(void))entry)();

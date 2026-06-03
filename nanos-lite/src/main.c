cb86e2c4b42d6c9cde6ffb4fdb7f522fca504e8a
tracer-ics2017 <tracer@njuics.org>
2026-06-03 17:52:17 +0800

    >  run 2413993 surius Linux Surius 6.17.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 19:30:42 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:52:17 up  2:04,  1 user,  load average: 0.53, 0.32, 0.27 6a0cc823bfed1df9983abf74d5db1a29b1d870db

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -26,7 +26,7 @@ int main() {
 #endif
   init_fs();
 
-  const char *program = "/bin/pal";
+  const char *program = "/bin/hello";
   Log("Loading program: %s", program);
   uint32_t entry = loader(NULL, program);
   ((void (*)(void))entry)();

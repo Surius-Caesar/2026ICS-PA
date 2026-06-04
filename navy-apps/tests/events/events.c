4ecf3617d2f5f12ec521ab39b97d8aa145e10e61
tracer-ics2017 <tracer@njuics.org>
2026-06-04 19:33:26 +0800

    >  run 2413993 surius Linux Surius 6.17.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 19:30:42 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:33:26 up  1:54,  1 user,  load average: 1.24, 0.71, 0.61 8ae5af379a7ffb4d2a6916bce2f18811ed933d5

--- navy-apps/tests/events/events.c
+++ navy-apps/tests/events/events.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
 
 int main() {
-  int fd = open("/dev/events", O_RDONLY);
+  int fd = open("/dev/events", 0);
   if (fd < 0) {
     printf("open /dev/events failed\n");
     return 1;
@@ -12,32 +11,24 @@ int main() {
 
   printf("events test: click NEMU window, then press keys\n");
 
-  char buf[256];
-  int n = 0;
-
   while (1) {
-    char c;
-    ssize_t r = read(fd, &c, 1);
-    if (r < 0) {
+    char buf[256];
+    char *p = buf;
+    ssize_t n;
+
+    while ((n = read(fd, p, 1)) > 0) {
+      if (*p == '\n') {
+        p[1] = '\0';
+        break;
+      }
+      p++;
+    }
+
+    if (p == buf) {
       continue;
     }
-    if (r == 0) {
-      printf("read returned 0\n");
-      continue;
-    }
-    if (c == '\0') {
-      printf("read null byte (events_read bug?)\n");
-      continue;
-    }
-    buf[n++] = c;
-    if (c == '\n') {
-      buf[n] = '\0';
-      printf("receive event: %s", buf);
-      n = 0;
-    }
-    if (n >= (int)sizeof(buf) - 1) {
-      n = 0;
-    }
+
+    printf("receive event: %s\n", buf);
   }
 
   return 0;

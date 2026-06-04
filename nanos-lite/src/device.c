b283143fb64437085072dcf9cb2dc53688dfa4ca
tracer-ics2017 <tracer@njuics.org>
2026-06-04 19:43:47 +0800

    >  run 2413993 surius Linux Surius 6.17.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 19:30:42 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:43:47 up  2:04,  1 user,  load average: 1.03, 0.60, 0.55 1264d9a1b581c9722efe79cabb34537f5678808f

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -13,60 +13,27 @@ char dispinfo[128] __attribute__((used));
 static char ev_buf[128];
 static size_t ev_len, ev_offset;
 
-#define KEYQ_SIZE 32
-static int keyq[KEYQ_SIZE];
-static int keyq_h, keyq_t;
-
-static void keyq_push(int key) {
-  int next = (keyq_t + 1) % KEYQ_SIZE;
-  if (next != keyq_h) {
-    keyq[keyq_t] = key;
-    keyq_t = next;
-  }
-}
-
-static int keyq_pop(void) {
-  if (keyq_h == keyq_t) {
-    return _KEY_NONE;
-  }
-  int key = keyq[keyq_h];
-  keyq_h = (keyq_h + 1) % KEYQ_SIZE;
-  return key;
-}
-
-static void keyq_drain(void) {
-  int key;
-  while ((key = _read_key()) != _KEY_NONE) {
-    keyq_push(key);
-  }
-}
-
-static void fill_event(void) {
-  ev_offset = 0;
-  keyq_drain();
-  int key = keyq_pop();
-  if (key != _KEY_NONE) {
-    int keycode = key & 0x7fff;
-    bool keydown = (key & 0x8000) != 0;
-    if (keycode > 0 && keycode < 256 && keyname[keycode] != NULL) {
-      snprintf(ev_buf, sizeof(ev_buf), "%s %s\n",
-          keydown ? "kd" : "ku", keyname[keycode]);
-    } else {
-      snprintf(ev_buf, sizeof(ev_buf), "t %u\n", (unsigned)_uptime());
-    }
-  } else {
-    snprintf(ev_buf, sizeof(ev_buf), "t %u\n", (unsigned)_uptime());
-  }
-  ev_len = strlen(ev_buf);
-}
-
 size_t events_read(void *buf, size_t len) {
   if (len == 0) {
     return 0;
   }
 
   if (ev_offset >= ev_len) {
-    fill_event();
+    int key = _read_key();
+    ev_offset = 0;
+    if (key != _KEY_NONE) {
+      int keycode = key & 0x7fff;
+      bool keydown = (key & 0x8000) != 0;
+      if (keycode > 0 && keycode < 256 && keyname[keycode] != NULL) {
+        snprintf(ev_buf, sizeof(ev_buf), "%s %s\n",
+            keydown ? "kd" : "ku", keyname[keycode]);
+      } else {
+        snprintf(ev_buf, sizeof(ev_buf), "t %u\n", (unsigned)_uptime());
+      }
+    } else {
+      snprintf(ev_buf, sizeof(ev_buf), "t %u\n", (unsigned)_uptime());
+    }
+    ev_len = strlen(ev_buf);
   }
 
   size_t n = len;
